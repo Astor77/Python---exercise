1f191aa06b2cef38f4e1bfc8d815a3f6943f20e8
Astor77 <164920255+Astor77@users.noreply.github.com>
2024-03-26 02:02:01 +0800

    Task #07 作業 03【實作題】奇偶數分堆
    #練習：奇偶數分堆
    
    ❏ Requirements：
    
    1. 建立一個列表（List ）初始化為 0, 1, 2... 9 的元素
    
    2. 請利用條件判斷與迴圈將 List 中的奇數放在前面、偶數放在後面
    
    3. 最後請將分堆後的結果印出
    
    ❏ Sample Input： [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] ← 預設初始化
    
    ❏ Sample Output： [1, 3, 5, 7, 9, 0, 2, 4, 6, 8] ← 畫面輸出
    
    ❏ Sample Code：
    
    L = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     
    '''
    Your Code
    '''
    
    print(L) # [1, 3, 5, 7, 9, 0, 2, 4, 6, 8]

--- Day07#03.py
+++ Day07#03.py
@@ -1,18 +1,13 @@
 L = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 odd_L = []
-
 even_L = []
 
 for n in L :
-
-if n % 2 == 0 :
-
-even_L.append(n)
-
-else :
-
-odd_L.append(n)
+    if n % 2 == 0 :
+        even_L.append(n)
+    else :
+        odd_L.append(n)
 
 L = odd_L + even_L
 
